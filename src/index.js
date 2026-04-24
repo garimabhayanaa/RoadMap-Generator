f125ab6ca042bd91194cd095e96cd4fed782d2b7
Garima Bhayana <garimaabhayanaa@gmail.com>
2026-04-25 01:17:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import CreateRoadmap from './pages/CreateRoadmaps';
 import NavBar from './components/NavBar';
 
 const navBar=ReactDOM.createRoot(document.getElementById('bar-area'));
