2ec51ad662864b38d2905f68b734ba43e954df6b
Garima Bhayana <garimaabhayanaa@gmail.com>
2026-04-26 00:46:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
+import CreateRoadmap from './pages/CreateRoadmaps';
 
 const navBar=ReactDOM.createRoot(document.getElementById('bar-area'));
 navBar.render(<NavBar/>);
@@ -12,6 +13,7 @@ root.render(
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/create" element={<CreateRoadmap />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
